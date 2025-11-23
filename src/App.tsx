48be767ff7b129691e59d2daec6842524acf709d
DaviTeixeira24 <davisilvateixeiramoreira@gmail.com>
2025-11-23 17:54:25 -0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import MainPage from "./pages/MainPage";
+import NavBar from "./components/NavBar";
+
+function App() {
+  return (
+    <>
+      <NavBar />
+      <MainPage />
+    </>
+  );
+}
+
+export default App;
